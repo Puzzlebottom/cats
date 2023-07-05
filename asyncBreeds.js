b25edea56395312ef755739193c009df2dafe8e1
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-04 21:01:21 -0400

    adds passing tests for asyncBreeds

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,15 +6,12 @@ const breedDetailsFromFile = function(breed, callback) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) callback(data);
+
+    if (error) callback(undefined);
+    else callback(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-const logDetails = (data) => {
-  console.log('Return Value: ', data);
-};
-
-// we try to get the return value
-breedDetailsFromFile('Bombay', logDetails);
+module.exports = breedDetailsFromFile;
